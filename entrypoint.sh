7ed5e51814979fbe32b730db781b6b31500dc539
Jan Wichelmann <j.wichelmann@uni-luebeck.de>
2023-04-14 15:09:54 +0200

    Pass DWARF path to action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
 scriptDirectory=$1
+dwarfPathPrefix=${2:-}
+
 cd $GITHUB_WORKSPACE/$scriptDirectory
 
 echo "Running build script..."
 bash build.sh
 
 echo "Running analysis script..."
-bash analyze.sh
+bash analyze.sh $dwarfPathPrefix
